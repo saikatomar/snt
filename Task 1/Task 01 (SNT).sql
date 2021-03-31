0e1487dc2d6335ea6b2daca07174ac00f41b8de6
Saikat Omar Khan <77507686+saikatomar@users.noreply.github.com>
2021-03-31 11:05:41 -0230

    Add files via upload

---
+++ Task 1/Task 01 (SNT).sql
@@ -0,0 +1,28 @@
+# Task 01: Create a visualization that provides a breakdown between the male and female 
+# employees working in the company each year, starting from 1990. 
+
+# We will choose from_date instead of hire_date because an employee could have changed
+# his/her dept during their career. 
+SELECT 
+	emp_no, from_date, to_date
+FROM 
+	t_dept_emp;
+
+SELECT distinct
+	emp_no, from_date, to_date
+FROM 
+	t_dept_emp;
+
+# The steps to check if there are any multipale record of same value
+# As it can be seen that both yeild for "331725 rows"
+
+SELECT
+	YEAR(de.from_date) AS Calendar_Year,
+	e.gender,
+    COUNT(e.emp_no) AS number_of_employees
+FROM
+	t_employees e
+		JOIN
+	t_dept_emp de ON de.emp_no = e.emp_no
+GROUP BY Calendar_Year, e.gender
+HAVING Calendar_year >= 1990;
